88273c9b5442966a6cadff7262c06fed32368e17
Tenjiroki <kryptorshow@gmail.com>
2025-07-25 14:07:59 +0300

    polishing and fixing

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TelegramWebhookController;
-
-Route::post('/webhook', [TelegramWebhookController::class, 'handle']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('welcome');
 });
